3065a0bf66a6eea83dde3ac81bdbe4db80b8d6a3
AndhikaGG <andikaafrianto26@gmail.com>
2025-10-07 08:42:38 +0700

    hemmp

--- lib/Types/Contact.d.ts
+++ lib/Types/Contact.d.ts
@@ -1,35 +1,23 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ContactAction = exports.Contact = void 0;
-
-const Contact = {
-  /** ID either in lid or jid format */
-  id: "",
-  /** ID in Lid (anonymous) format (@lid) */
-  lid: undefined,
-  /** ID in Phone Number format (@s.whatsapp.net) */
-  jid: undefined,
-  /** name of the contact, you have saved on your WA */
-  name: undefined,
-  /** name of the contact, the contact has set on their own on WA */
-  notify: undefined,
-  /** I have no idea */
-  verifiedName: undefined,
-  /**
-   * Url of the profile picture of the contact
-   *
-   * 'changed' => if the profile picture has changed
-   * null => if the profile picture has not been set (default profile picture)
-   * any other string => url of the profile picture
-   */
-  imgUrl: undefined,
-  status: undefined,
-};
-exports.Contact = Contact;
-
-const ContactAction = {
-  firstName: "",
-  fullName: "",
-  saveOnPrimaryAddressbook: false,
-};
-exports.ContactAction = ContactAction;
+export interface Contact {
+    /** ID either in lid or jid format **/
+    id: string;
+    /** ID in Lid (anonymous) format (@lid) **/
+    lid?: string;
+    /** ID in Phone Number format (@s.whatsapp.net)  **/
+    jid?: string;
+    /** name of the contact, you have saved on your WA */
+    name?: string;
+    /** name of the contact, the contact has set on their own on WA */
+    notify?: string;
+    /** I have no idea */
+    verifiedName?: string;
+    /**
+     * Url of the profile picture of the contact
+     *
+     * 'changed' => if the profile picture has changed
+     * null => if the profile picture has not been set (default profile picture)
+     * any other string => url of the profile picture
+     */
+    imgUrl?: string | null;
+    status?: string;
+}
