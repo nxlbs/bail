a7226e1585dbf1a40592b18be0b28d46c41587fd
AndhikaGG <andikaafrianto26@gmail.com>
2025-10-07 08:10:33 +0700

    hemmp

--- lib/Utils/use-mongo-file-auth-state.d.ts
+++ lib/Utils/use-mongo-file-auth-state.d.ts
@@ -1,5 +1,5 @@
-import { AuthenticationState } from '../Types'
+import { AuthenticationState } from "../Types";
 export declare const useMongoFileAuthState: (collection: string) => Promise<{
-state: AuthenticationState
-saveCreds: () => Promise<void>
-}>
+  state: AuthenticationState;
+  saveCreds: () => Promise<void>;
+}>;
--- lib/WABinary/encode.d.ts
+++ lib/WABinary/encode.d.ts
@@ -1,2 +1,6 @@
-import type { BinaryNode, BinaryNodeCodingOptions } from './types'
-export declare const encodeBinaryNode: (node: BinaryNode, opts?: Pick<BinaryNodeCodingOptions, 'TAGS' | 'TOKEN_MAP'>, buffer?: number[]) => Buffer
+import type { BinaryNode, BinaryNodeCodingOptions } from "./types";
+export declare const encodeBinaryNode: (
+  node: BinaryNode,
+  opts?: Pick<BinaryNodeCodingOptions, "TAGS" | "TOKEN_MAP">,
+  buffer?: number[],
+) => Buffer;
